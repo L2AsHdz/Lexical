51da801d31fa44496d51e676636f42f4b78f231b
LAHdz <leonidas0197@gmail.com>
2018-10-07 11:35:45 -0600

    antes de pruebas en el default code

---
+++ src/lexicalanalyzer/LexicalAnalyzer.java
@@ -0,0 +1,19 @@
+package lexicalanalyzer;
+
+import javax.swing.JFrame;
+import lexicalanalyzer.ui.Interfaz;
+
+/**
+ *
+ * @author asael
+ */
+public class LexicalAnalyzer {
+
+    public static void main(String[] args) {
+        Interfaz ui = new Interfaz();
+        ui.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        ui.setLocationRelativeTo(null);
+        ui.setVisible(true);
+    }
+    
+}
