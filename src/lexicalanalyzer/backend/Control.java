35929b393f15679f2327d58186a34ce73671f486
LAHdz <leonidas0197@gmail.com>
2018-10-08 01:50:13 -0600

    prueba externa

--- src/lexicalanalyzer/backend/Control.java
+++ src/lexicalanalyzer/backend/Control.java
@@ -10,7 +10,6 @@ public class Control {
                             'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
                             'P','Q','R','S','T','U','V','W','X','Y','Z'};
     private final char[] DIGITOS = {'0','1','2','3','4','5','6','7','8','9'};
-    private final char[] OP_ARITMETICOS = {'+','-','*','/','%'};
     private final char[] SIGNOS_AGRUPACION = {'(',')','{','}','[',']'};
     private final char[] SIGNOS_PUNTUACION = {',','.',';',':'};
     
